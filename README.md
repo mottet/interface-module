bed6d24f2595b4abde302501a0f08bb033f73fd0
Marvin Mottet <marvin.mottet@epitech.eu>
2017-07-03 22:27:20 +0200

    Creation of the web application
    Creation of the web application thinks to create-react-app. Redux and
    Redux-saga installed. First build able to run without any problem.

---
+++ README.md
@@ -0,0 +1,13 @@
+#Interface Module
+
+The goal here is to create a small web application that provides a user interface for educative modules. It needs to use React, Redux, Redux-saga and have to follow ES6.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+### Running
+
+To run the web application, open a Terminal in the `build` folder and type:
+
+`python -m SimpleHTTPServer 2000`
+
+Then, open `http://localhost:2000/` in a internet browser.
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Work in progress</h2>
+        </div>
+        <p className="App-intro">
+          Bientôt ici un petit test d'une application React.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
